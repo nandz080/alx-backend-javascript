9ad0bfc3b7469d98fc9d7bfe0d4ad8e95b4a8068
nandz080 <nandz080@gmail.com>
2024-04-11 22:22:40 +0000

    [task 6] Import Building from 5-building.js Implement a class named SkyHighBuilding that extends from Building

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,30 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    if (typeof floors !== 'number') throw new Error();
+    super(sqft);
+    this._floors = floors;
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    this._sqft = newSqft;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(newFloors) {
+    this._floors = newFloors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
