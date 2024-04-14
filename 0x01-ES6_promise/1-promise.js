e48be7cc371cbb475fbaeea0ac6f01c03de1ca67
nandz080 <nandz080@gmail.com>
2024-04-14 19:59:21 +0000

    [task 1] Using the prototype below, return a promise. The parameter is a boolean

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -5,7 +5,7 @@ export default function getFullResponseFromAPI(success) {
 		}
 
 		else{
-			reject({Error: "The fake API is not working currently"});
+			reject(Error( "The fake API is not working currently"));
 		}
 	});
 }
