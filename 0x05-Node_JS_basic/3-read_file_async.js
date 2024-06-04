b66625c9dfb635fa4848c03ab38ff8761dc03628
nandz080 <nandz080@gmail.com>
2024-06-04 21:34:42 +0000

    [task 3] Create a function named constStudents

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,37 +1,31 @@
 const fs = require('fs');
 
-function countStudents(path) {
-    return new Promise((resolve, reject) => {
-        fs.readFile(path, 'utf8', (error, data) => {
-            if (error) {
-                reject(new Error('Cannot load the database'));
-            } else {
-                const lines = data.split('\n').filter(line => line.trim() !== ''); // Filter out empty lines
-                const students = lines.slice(1); // Exclude the header line
-                const studentsByField = {};
-
-                students.forEach(student => {
-                    const fields = student.split(',');
-                    const field = fields[3].trim();
-
-                    if (!(field in studentsByField)) {
-                        studentsByField[field] = [];
-                    }
-
-                    studentsByField[field].push(fields[0].trim());
-                });
-
-                const totalStudents = students.length;
-                console.log(`Number of students: ${totalStudents}`);
-
-                Object.entries(studentsByField).forEach(([field, students]) => {
-                    console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
-                });
-
-                resolve();
-            }
+async function countStudents(path) {
+  if (fs.existsSync(path)) {
+    return new Promise((resolve) => {
+      fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+          throw Error('Cannot load the database');
+        }
+        const output = [];
+        data.split('\n').forEach((data) => {
+          output.push(data.split(','));
         });
+        output.shift();
+        const lines = [];
+        output.forEach((data) => lines.push([data[0], data[3]]));
+        const field = new Set();
+        lines.forEach((item) => field.add(item[1]));
+        const final = {};
+        field.forEach((data) => { (final[data] = 0); });
+        lines.forEach((data) => { (final[data[1]] += 1); });
+        console.log(`Number of students: ${output.filter((check) => check.length > 3).length}`);
+        Object.keys(final).forEach((data) => console.log(`Number of students in ${data}: ${final[data]}. List: ${lines.filter((n) => n[1] === data).map((n) => n[0]).join(', ')}`));
+        resolve(output, final, lines);
+      });
     });
+  }
+  throw Error('Cannot load the database');
 }
 
 module.exports = countStudents;
